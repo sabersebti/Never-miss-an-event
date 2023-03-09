ecaf4c450891558e28fccd314d4e3f49cddf79f8
saber sebti <sebti.saber1@gmail.com>
2023-03-09 22:52:09 +0000

    fixed the date display

--- script.js
+++ script.js
@@ -1,50 +1,24 @@
-$(document).ready(function(){
-    let currentDay = $("#currentDay");
-    let date = moment().format("dddd, MMMM Do YYYY");
-    currentDay.text(date);
-  });
-  $(document).ready(function(){
-    let container = $(".container");
-    let businessHours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-    
-    for (let i = 0; i < businessHours.length; i++) {
-      let row = $("<div>", {
-        class: "row time-block"
-      });
-      row.append(`
-        <div class="hour col-2">${businessHours[i]}</div>
-        <textarea class="col-8 description"></textarea>
-        <button class="saveBtn col-2"><i class="fas fa-save"></i></button>
-      `);
-      container.append(row);
+
+
+function updateScheduleView() {
+  let currentDate = moment().format("dddd, MMMM Do YYYY");
+  let currentHour = moment().format("H"); 
+  $(".time-block").each(function() {
+    let hour = $(this).find(".hour").text().slice(0,-2);
+    if(hour == currentHour){
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } else if (hour < currentHour) {
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
     }
   });
-  $(document).ready(function(){
-    let timeBlocks = $(".time-block");
-    let currentHour = moment().format("h");
-    
-    timeBlocks.each(function(){
-      let hour = $(this).find(".hour").text().slice(0,-2);
-      if(hour == currentHour){
-        $(this).addClass("present");
-      } else if (hour < currentHour) {
-        $(this).addClass("past");
-      } else {
-        $(this).addClass("future");
-      }
-    });
-  });
-  $(document).ready(function(){
-    $(".time-block").on("click", function(){
-      $(this).find(".description").removeAttr("readonly");
-    });
-  });
-  $(document).ready(function(){
-    $(".saveBtn").on("click", function(){
-      let hour = $(this).parent().find(".hour").text();
-      let description = $(this).parent().find(".description").val();
-      localStorage.setItem(hour, description);
-    });
-  });
-      
+}
+
+updateScheduleView(); //call the function to update colors initially
+
+
       
