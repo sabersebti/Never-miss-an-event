617ef813b6d7071019ce48c7cc8c7ab62dab3e03
saber sebti <sebti.saber1@gmail.com>
2023-03-10 15:09:46 +0000

    finally i maaged to fix the colour coding

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
+$(document).ready(function() {
 
-
-function updateScheduleView() {
+  // Get current date using Moment.js
   let currentDate = moment().format("dddd, MMMM Do YYYY");
-  let currentHour = moment().format("H"); 
-  $(".time-block").each(function() {
-    let hour = $(this).find(".hour").text().slice(0,-2);
-    if(hour == currentHour){
-      $(this).removeClass("past");
-      $(this).addClass("present");
-    } else if (hour < currentHour) {
-      $(this).removeClass("present");
-      $(this).addClass("past");
-    } else {
-      $(this).removeClass("past");
-      $(this).removeClass("present");
-    }
-  });
+  
+  // Set text of #currentDay element to currentDate
+  $('#currentDay').text(currentDate);
+
+for (let i = 9; i <= 17; i++) {
+  let hour = i;
+  let $timeBlock = $('<div class="row time-block">');
+  let $hourCol = $(`<div class="hour col-2">${hour}:00</div>`);
+  let $descriptionCol = $('<textarea class="col-8 description"></textarea>');
+  let $saveBtnCol = $('<button class="saveBtn col-2"><i class="fas fa-save"></i></button>');
+
+  $timeBlock.append($hourCol, $descriptionCol, $saveBtnCol);
+  $('.container').append($timeBlock);
 }
 
-updateScheduleView(); //call the function to update colors initially
+
+  $(".time-block").on("click", function() {
+    // Find the textarea within the clicked time block and give it focus
+    $(this).find("textarea").focus();
+  });
+
+  $(".saveBtn").on("click", function() {
+    // Get the text from the textarea
+    var text = $(this).siblings(".description").val().trim();
+
+    // Get the hour for this timeblock
+    var hour = $(this).siblings(".hour").text().slice(0,-2);
+
+    // Save the event to local storage
+    localStorage.setItem(hour, text);
+  });
+
+  function updateScheduleView() {
+    let currentDate = moment().format("dddd, MMMM Do YYYY");
+    let currentHour = moment().format("H"); 
+    $(".time-block").each(function() {
+      let hour = parseInt($(this).children(".hour").text());
+      let $this = $(this);
+      $this.toggleClass("present", hour == currentHour)
+        .toggleClass("past", hour < currentHour)
+        .toggleClass("future", hour > currentHour);	  
+    });
+  }
+  
+  // Call the updateScheduleView function to color-code time blocks 
+  updateScheduleView(); 
+
+});
 
 
       
