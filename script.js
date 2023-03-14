a386c5c5205d457548809a33086b6d52613f8949
saber sebti <sebti.saber1@gmail.com>
2023-03-14 12:57:31 +0000

    fixed the local storage issue

--- script.js
+++ script.js
@@ -1,78 +1,92 @@
-let autoUpdateTimer;
+const $container = $(".container");
+const $currentDay = $("#currentDay");
+const $headerMessage = $("#header-message");
 
-$(document).ready(function () {
-  // Get current date using Moment.js
-  let currentDate = moment().format("dddd, MMMM Do YYYY");
-  $('#currentDay').text(currentDate);
 
-  // Create time blocks for each hour of the day and append to .container div
-  for (let i = 9; i <= 17; i++) {
-    let $timeBlock = generateTimeBlock(i);
-    $('.container').append($timeBlock);
-  }
 
-  // Find the textarea within the clicked time block and give it focus
-$(".time-block").on("click", function(event) {
-  $(event.currentTarget).find("textarea").focus();
-});
 
-updateScheduleView();
-
-// Update the row color based on past, present, and future time
-function updateScheduleView() {
-  var currentTime = moment();
+const updateScheduleView = () => {  
+  const currentTime = moment();
 
   $(".time-block").each(function () {
-    var blockTime = moment($(this).children(".hour").text(), ["H:mm"]);
+    const $this = $(this);
+    const blockTime = moment($this.find(".hour").text(), [timeFormats.hour]);
 
     if (blockTime.isBefore(currentTime, "hour")) {
-      $(this).removeClass("present future").addClass("past");
+      $this.removeClass("present future").addClass("past");
     } else if (blockTime.isSame(currentTime, "hour")) {                
-      $(this).removeClass("past future").addClass("present");
+      $this.removeClass("past future").addClass("present");
     } else {         
-      $(this).removeClass("past present").addClass("future");
+      $this.removeClass("past present").addClass("future");
     }
   });
 }
 
- // If the timer is not already set up, start it
- if (!autoUpdateTimer) {
-  startAutoUpdate();
-}
-})
+const timeFormats = {
+  hour: "H:mm",
+  dayOfWeek: "dddd",
+  monthDayYear: "MMMM Do YYYY",
+};
 
-function startAutoUpdate() {
-  clearInterval(autoUpdateTimer);
-  autoUpdateTimer = setInterval(function() {
-    updateScheduleView();
-  }, 60 * 1000); // Update every minute (60 seconds times 1000 milliseconds)
-}
-
-
-$("body").on("click", ".saveBtn", function() {
-  let text = $(this).siblings(".description").val().trim();
-  let hour = +$(this).siblings(".hour").data("hour");
-  const messageDelay = 5000;
-
-  if (text !== "" && !isNaN(hour) && hour >= 0 && hour <= 23) {
-    const eventData = { hour, text };
-    localStorage.setItem(hour, JSON.stringify(eventData));
-    $('#header-message').text('Data has been saved.');
-    setTimeout(() => $('#header-message').text(''), messageDelay);
-    updateScheduleView();
-    startAutoUpdate();
-  } else {
-    alert('Invalid input!');
+const generateTimeBlock = (hour) => {
+  const formattedHour = moment({ hour }).format(timeFormats.hour);
+  const data = localStorage.getItem(hour);
+  let eventData = {};
+  if (data) {
+    try {
+      eventData = JSON.parse(data);
+    } catch (e) {
+      console.error(`Error parsing data for hour ${hour}:`, e);
+    }
   }
-});
 
-function generateTimeBlock(hour) {
-  let $timeBlock = $('<div class="row time-block">');
-  let formattedHour = moment({ hour: hour }).format("H:mm");
-  let $hourCol = $(`<div class="hour col-2" data-hour="${hour}">${formattedHour}</div>`);
-  let data = localStorage.getItem(formattedHour);
-  let $descriptionCol = $('<textarea class="col-8 description"></textarea>').val(data);
-  let $saveBtnCol = $('<button class="saveBtn col-2"><i class="fas fa-save"></i></button>');
+  const $timeBlock = $(`<div class="row time-block"></div>`);
+  const $hourCol = $(`<div class="hour col-2" data-hour="${hour}">${formattedHour}</div>`);
+  const $descriptionCol = $('<textarea class="col-8 description"></textarea>').val(eventData.text);
+  const $saveBtnCol = $('<button class="saveBtn col-2"><i class="fas fa-save"></i></button>');
 
   return $timeBlock.append($hourCol, $descriptionCol, $saveBtnCol);
-}
+};
+
+
+$(() => {
+  // Cache DOM elements
+  const $body = $("body");
+  const $container = $(".container");
+  const $currentDay = $("#currentDay");
+
+  // Get current date using Moment.js
+  const currentDate = moment().format(`${timeFormats.dayOfWeek}, ${timeFormats.monthDayYear}`);
+  $currentDay.text(currentDate);
+
+  // Create time blocks for each hour of the day and append to .container div
+  const $timeBlocks = [];
+  for (let i = 9; i <= 17; i++) {
+    $timeBlocks.push(generateTimeBlock(i));
+  }
+  $container.append($timeBlocks);
+
+  // Attach event listeners using delegation
+  $body.on("click", ".time-block", function () {
+    $(this).find("textarea").focus();
+  });
+
+  $body.on("click", ".saveBtn", function () {
+    const $this = $(this);
+    const $descriptionCol = $this.siblings(".description");
+    const text = $descriptionCol.val().trim();
+    const hour = +$this.siblings(".hour").data("hour");
+    const eventData = { hour, text };
+localStorage.setItem(hour, JSON.stringify(eventData || {}));
+    $headerMessage.text("Data has been saved.");
+    setTimeout(() => {
+      $headerMessage.text("");
+    }, 5000);
+
+    updateScheduleView();
+  });
+
+  // Update schedule view and start auto-update timer
+  updateScheduleView();
+  setInterval(updateScheduleView, 60 * 1000);
+});
